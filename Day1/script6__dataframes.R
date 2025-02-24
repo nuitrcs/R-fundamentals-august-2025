a59016063ea8acbfce2b64783bc23cb1feb4293a
Emilio Lehoucq <emiliolehoucq@gmail.com>
2025-02-24 09:22:26 -0600

    added qog data and documentation and changed day 1

--- Day1/script6__dataframes.R
+++ Day1/script6__dataframes.R
@@ -1,25 +1,25 @@
 # Working with data frames
 
 # reading in a data file --------
-evp <- read.csv(file="data/ev_police_jan.csv")
+qog <- read.csv(file="data/qog_data.csv")
 
 # it has columns and rows. Let's look at it
-View(evp)
+View(qog)
 
-ncol(evp) # number of columns/variables in data frame
-nrow(evp) # number of rows/observations in data frame
+ncol(qog) # number of columns/variables in data frame
+nrow(qog) # number of rows/observations in data frame
 
 
 # what are the variables?
-names(evp)
+names(qog)
 
 
 # each column is a vector that can be accessed with $
-evp$subject_race
-evp$subject_sex
+qog$cname_qog
+qog$year
 
 
-table(evp$vehicle_make) # operations on vectors
+table(qog$cname_qog) # operations on vectors
 
 
 
@@ -27,26 +27,26 @@ table(evp$vehicle_make) # operations on vectors
 # Indexing : dataframe[ row indices, col indices ] --------
 
 ## indexing by position --------
-evp[1,1]
-evp[1,]
-evp[,1]
+qog[1,1]
+qog[1,]
+qog[,1]
 
-evp[1:10, ]
+qog[1:10, ]
 
 
 ## indexing by name --------
-evp[, "location"]
-evp[, c("subject_sex", "subject_race", "location")]
+qog[, "cname_qog"]
+qog[, c("cname_qog", "year")]
 
 
 ## boolean indexing --------
-evp[evp$vehicle_make == "CADI", ]
-evp[evp$vehicle_year > 2010, ]
+qog[qog$cname_qog == "United States", ]
+qog[qog$year > 2010, ]
 
-evp[evp$vehicle_make == "CADI", "vehicle_year" ] # row and column indexing with boolean condition
+qog[qog$cname_qog == "United States", "ht_region" ] # row and column indexing with boolean condition
 
 
 # make new columns in a data frame --------
-evp$vehicle_age_in_2024 <- 2024 - evp$vehicle_year
-View(evp)
-names(evp)
+qog$cckp_rain_cm <- qog$cckp_rain / 10
+View(qog)
+names(qog)
